ee1b20071cdb01b98e2eaa74222f695ded84a951
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-28 20:12:21 +0530

    codeforces

---
+++ vowels.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    string a;
+    cin >> a;
+    
+    if (a == "a" or a =="e" or a=="i" or a =="o" or a =="u"){
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO"<< endl;
+    }
+    
+    
+    return 0;
+}
