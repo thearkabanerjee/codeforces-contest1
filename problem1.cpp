2bd5254bc972d3c45c3d19eec7af3dd76999d65a
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-28 20:08:00 +0530

    codeforces

--- problem1.cpp
+++ problem1.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 
 int main(){
-    
+    int a;
+    cin >> a;
+    for (int i = 1; i <=a; i++){
+        cout << "Hello Codeforces " <<  i<< endl;
+    }
     
     return 0;
 }
